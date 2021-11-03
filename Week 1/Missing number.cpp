473bdaaacc7275c30b83ec6c94dff459dce7916f
itsmona14 <77101592+itsmona14@users.noreply.github.com>
2021-11-03 20:49:37 +0530

    Create Missing number.cpp

---
+++ Week 1/Missing number.cpp
@@ -0,0 +1,13 @@
+https://leetcode.com/problems/missing-number/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(i != nums[i])
+                return i;
+        }
+        return nums[nums.size()-1]+1;
+    }
+};
