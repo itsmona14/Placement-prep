ba9e5a2cdb8230d24a5fa1f68edaad23dc2628fa
itsmona14 <77101592+itsmona14@users.noreply.github.com>
2021-11-03 15:30:15 +0530

    Create Two sum.cpp

---
+++ Week 1/Two sum.cpp
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/two-sum
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i = 0 ; i < nums.size() ; i++){
+            for(auto j = i + 1 ; j < nums.size() ; j++){
+                if((nums[i] + nums[j]) == target){
+                    ans.push_back(i);   
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
+};
