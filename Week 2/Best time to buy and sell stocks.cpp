3500823532faafd0079d27bd97fd6a3f421f0a6e
Monashree K <77101592+itsmona14@users.noreply.github.com>
2021-11-13 14:40:46 +0530

    Create Best time to buy and sell stocks.cpp

---
+++ Week 2/Best time to buy and sell stocks.cpp
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int mini = prices[0];
+        for(int i = 0; i < prices.size(); i++){
+            mini = min(mini,prices[i]);
+            profit = max(prices[i] - mini,profit);
+        }
+        
+        return profit;
+    }
+};
