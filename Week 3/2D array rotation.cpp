5f6fcd79b71d66a2af11f3b2c949d3f95303eb8b
Monashree K <77101592+itsmona14@users.noreply.github.com>
2021-11-19 22:57:32 +0530

    Create 2D array rotation.cpp

---
+++ Week 3/2D array rotation.cpp
@@ -0,0 +1,10 @@
+https://practice.geeksforgeeks.org/problems/rotate-a-2d-array-without-using-extra-space1004/1
+
+For clockwise rotate:
+1)Transpose the matrix
+2)Reverse every row
+
+For Anti-clockwise rotate:
+1)Reverse every row
+2)Transpose the matrix
+
