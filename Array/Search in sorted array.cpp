ed3972f09a85bc0ba970276984b1b5471304dc2a
Monashree K <77101592+itsmona14@users.noreply.github.com>
2022-07-09 00:35:21 +0530

    Create Search in sorted array.cpp

---
+++ Array/Search in sorted array.cpp
@@ -0,0 +1,33 @@
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start = 0; 
+        int mid;
+        int end = nums.size()-1;
+        while(start <= end){
+            mid = (start+end)/2;
+            if(target == nums[mid]){
+                return mid;
+            }
+            else if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target <= nums[mid]){
+                    end = mid-1;
+                }
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(nums[mid] <= target && target <= nums[end]){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+};
